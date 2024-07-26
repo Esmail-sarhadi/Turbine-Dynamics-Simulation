c54021b49a839ba799eea58ee66bb66b10d31422
Esmail Sarhadi <45729949+Esmail-sarhadi@users.noreply.github.com>
2024-07-26 13:22:48 +0330

    Delete tests directory

--- tests/test_my_module.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from my_package.my_module import some_function
-
-class TestMyModule(unittest.TestCase):
-    def test_some_function(self):
-        self.assertEqual(some_function(2, 3), 5)
-
-if __name__ == '__main__':
-    unittest.main()
